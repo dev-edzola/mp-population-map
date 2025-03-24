e222d7bbecfde0a01641c64ba2e25e66208518d1
dev-edzola <developer@edzola.com>
2025-03-24 19:01:47 +0530

    first push

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 export default defineConfig({
-  base: '/mp-population-map/', // 👈 Set your GitHub repo name here
+  base: '/mp-population-map/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [react(),componentTagger()],
 })
 
-    
- 
