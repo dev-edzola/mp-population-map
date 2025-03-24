26ccbc7ccca0970772c5b22571d4f2ea1f7fa7aa
dev-edzola <developer@edzola.com>
2025-03-24 18:58:55 +0530

    first push

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  base: '/mp-population-map/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/mp-population-map/', // 👈 Set your GitHub repo name here
+  plugins: [react(),componentTagger()],
+})
+
+    
+ 
